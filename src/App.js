7fbed5e83c4906a0e4fcca0f8733f1243a14205a
John Seyfert <johnseyfert@Johns-MBP.attlocal.net>
2020-04-24 14:30:46 -0400

    added gaugr content

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import './product.css';
 // import { coffee } from "@fortawesome/free-solid-svg-icons";
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin, faInstagram, faSpotify } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faInstagram, faSpotify, faApple, faAndroid } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
@@ -11,21 +11,31 @@ function App() {
     <div className="App">
       <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
         <div className="col-md-5 p-lg-5 mx-auto my-5">
-          <img className="img-thumbnail rounded-circle" src="/avatar3.jpg" style={{height: '33vh'}} alt="Avatar" />
+          <img className="img-thumbnail rounded-circle" src="/avatar3.jpg" style={{height: '30vh', width: '30vh'}} alt="Avatar" />
           <h1 className="display-4 font-weight-normal">John Seyfert</h1>
-          <p className="lead font-weight-normal">Full Stack Developer</p>
-          
+          <h4 className="display-5">
+            <span style={{color: '#6c757d'}}>{'{ '}</span>
+            Full Stack Developer
+            <span style={{color: '#6c757d'}}>{' }'}</span>
+          </h4>
+          <br/>
+          <p className="lead">I work with React, React-Native, Node, Express, Firebase and SQL/NoSQL.<br/>Scroll down to see what I've been up to.</p>
+          {/* <p className="lead">Below are some of my projects built with<br/>React, React-Native, Node, Express, Firebase.</p> */}
+          {/* <p className="lead">I specialize in React and React-Native in the front, Node and Firebase in the back.<br/>To see what i have been up to scroll down.</p> */}
+          {/* <p className="lead">Full stack developer since 2016. My currecnt focus is React and React-Native :: Node and Firebase</p> */}
+          <br/>
+          <br/>
           <div>
-            <a href="https://github.com/jseyfert" target="_blank" rel="noopener noreferrer" style={{marginRight: '10px', color: '#6c757d'}}>
+            <a href="https://github.com/jseyfert" target="_blank" rel="noopener noreferrer" style={{marginRight: '33px', color: '#6c757d'}}>
               <FontAwesomeIcon icon={faGithub} size="2x"/>
             </a>
-            <a href="https://www.linkedin.com/in/johnseyfert/" target="_blank" rel="noopener noreferrer" style={{marginRight: '10px', color: '#6c757d'}}>
+            <a href="https://www.linkedin.com/in/johnseyfert/" target="_blank" rel="noopener noreferrer" style={{marginRight: '33px', color: '#6c757d'}}>
               <FontAwesomeIcon icon={faLinkedin} size="2x"/>
             </a>
-            <a href="https://www.instagram.com/johnny.montana/" target="_blank" rel="noopener noreferrer" style={{marginRight: '10px', color: '#6c757d'}}>
+            <a href="https://www.instagram.com/johnny.montana/" target="_blank" rel="noopener noreferrer" style={{marginRight: '33px', color: '#6c757d'}}>
             <FontAwesomeIcon icon={faInstagram} size="2x"/>
             </a>
-            <a href="https://open.spotify.com/user/1232230335?si=nmj9HnOZRRaTkYeRdvleAg" target="_blank" rel="noopener noreferrer" style={{marginRight: '10px', color: '#6c757d'}}>
+            <a href="https://open.spotify.com/user/1232230335?si=nmj9HnOZRRaTkYeRdvleAg" target="_blank" rel="noopener noreferrer" style={{marginRight: '33px', color: '#6c757d'}}>
               <FontAwesomeIcon icon={faSpotify} size="2x"/>
             </a>
             <a href="mailto:johnseyfert@gmail.com" target="_blank" rel="noopener noreferrer" style={{color: '#6c757d'}}>
@@ -40,20 +50,55 @@ function App() {
       </div>
 
       <div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+
         <div className="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
           <div className="my-3 py-3">
-            <h2 className="display-5">Another headline</h2>
-            <p className="lead">And an even wittier subheading.</p>
+            <h2 className="display-5">Easy Alfred</h2>
+            <p className="lead">Designed and built a premium concierge web app with React and Firebase....... ect asdf sadf asdf</p>
+            <div>
+              <a href="https://app.easyalfred.com/" target="_blank" rel="noopener noreferrer" style={{marginRight: '33px', color: '#6c757d'}}>
+                <FontAwesomeIcon icon={faExternalLinkSquareAlt} size="2x"/>
+              </a>
+              <a href="https://github.com/jseyfert/easyAlfred" target="_blank" rel="noopener noreferrer" style={{marginRight: '33px', color: '#6c757d'}}>
+                <FontAwesomeIcon icon={faGithub} size="2x"/>
+              </a>
+              {/* <a href="https://github.com/jseyfert/fuse-react" target="_blank" rel="noopener noreferrer" style={{marginRight: '33px', color: '#6c757d'}}>
+                <FontAwesomeIcon icon={faInfoCircle} size="2x"/>
+              </a> */}
+            </div>
           </div>
-          <div className="bg-light shadow-sm mx-auto" style={{width: "80%", height: '300px', borderRadius: '21px 21px 0 0'}}></div>
+          <div 
+          className="bg-light shadow-sm mx-auto" 
+          style={{
+            width: "90%", 
+            height: '300px', 
+            borderRadius: '21px 21px 0 0',
+            backgroundImage: 'url(/easyAlfred1.png)',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+          }}
+          ></div>
         </div>
+
         <div className="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
           <div className="my-3 p-3">
-            <h2 className="display-5">Another headline</h2>
-            <p className="lead">And an even wittier subheading.</p>
+            <h2 className="display-5">Gaugr</h2>
+            <p className="lead">Regularly contract for this music intelligence app built with React-Naive and Firebase, fixing bugs and extending features.</p>
+            <div>
+              <a href="https://gaugr.com/" target="_blank" rel="noopener noreferrer" style={{marginRight: '33px', color: '#6c757d'}}>
+                <FontAwesomeIcon icon={faExternalLinkSquareAlt} size="2x"/>
+              </a>
+              <a href="https://apps.apple.com/us/app/gaugr/id1323225732" target="_blank" rel="noopener noreferrer" style={{marginRight: '33px', color: '#6c757d'}}>
+                <FontAwesomeIcon icon={faApple} size="2x"/>
+              </a>
+              <a href="https://play.google.com/store/apps/details?id=com.gaugr&hl=en_US" target="_blank" rel="noopener noreferrer" style={{marginRight: '33px', color: '#6c757d'}}>
+                <FontAwesomeIcon icon={faAndroid} size="2x"/>
+              </a>
+            </div>
           </div>
-          <div className="bg-dark shadow-sm mx-auto" style={{width: "80%", height: '300px', borderRadius: '21px 21px 0 0'}}></div>
+          <div className="bg-dark shadow-sm mx-auto gaugrBackground"></div>
         </div>
+
       </div>
 
       <div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
@@ -62,14 +107,14 @@ function App() {
             <h2 className="display-5">Another headline</h2>
             <p className="lead">And an even wittier subheading.</p>
           </div>
-          <div className="bg-dark shadow-sm mx-auto" style={{width: "80%", height: '300px', borderRadius: '21px 21px 0 0'}}></div>
+          <div className="bg-dark shadow-sm mx-auto" style={{width: "90%", height: '300px', borderRadius: '21px 21px 0 0'}}></div>
         </div>
         <div className="bg-primary mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
           <div className="my-3 py-3">
             <h2 className="display-5">Another headline</h2>
             <p className="lead">And an even wittier subheading.</p>
           </div>
-          <div className="bg-light shadow-sm mx-auto" style={{width: "80%", height: '300px', borderRadius: '21px 21px 0 0'}}></div>
+          <div className="bg-light shadow-sm mx-auto" style={{width: "90%", height: '300px', borderRadius: '21px 21px 0 0'}}></div>
         </div>
       </div>
 
@@ -79,14 +124,14 @@ function App() {
             <h2 className="display-5">Another headline</h2>
             <p className="lead">And an even wittier subheading.</p>
           </div>
-          <div className="bg-white shadow-sm mx-auto" style={{width: "80%", height: '300px', borderRadius: '21px 21px 0 0'}}></div>
+          <div className="bg-white shadow-sm mx-auto" style={{width: "90%", height: '300px', borderRadius: '21px 21px 0 0'}}></div>
         </div>
         <div className="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
           <div className="my-3 py-3">
             <h2 className="display-5">Another headline</h2>
             <p className="lead">And an even wittier subheading.</p>
           </div>
-          <div className="bg-white shadow-sm mx-auto" style={{width: "80%", height: '300px', borderRadius: '21px 21px 0 0'}}></div>
+          <div className="bg-white shadow-sm mx-auto" style={{width: "90%", height: '300px', borderRadius: '21px 21px 0 0'}}></div>
         </div>
       </div>
 
@@ -96,14 +141,14 @@ function App() {
             <h2 className="display-5">Another headline</h2>
             <p className="lead">And an even wittier subheading.</p>
           </div>
-          <div className="bg-white shadow-sm mx-auto" style={{width: "80%", height: '300px', borderRadius: '21px 21px 0 0'}}></div>
+          <div className="bg-white shadow-sm mx-auto" style={{width: "90%", height: '300px', borderRadius: '21px 21px 0 0'}}></div>
         </div>
         <div className="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
           <div className="my-3 py-3">
             <h2 className="display-5">Another headline</h2>
             <p className="lead">And an even wittier subheading.</p>
           </div>
-          <div className="bg-white shadow-sm mx-auto" style={{width: "80%", height: '300px', borderRadius: '21px 21px 0 0'}}></div>
+          <div className="bg-white shadow-sm mx-auto" style={{width: "90%", height: '300px', borderRadius: '21px 21px 0 0'}}></div>
         </div>
       </div>
 
